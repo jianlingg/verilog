a7472bbdcae2833a56e1deb88357c6518eabc536
jianlingg <yunjicangtian@gmail.com>
2023-04-19 01:12:03 +0800

    1

--- data_clock/rst.v
+++ data_clock/rst.v
@@ -31,3 +31,24 @@ always  @(posedge clk or negedge rst_n)begin
 end
     
 endmodule
+
+//格雷码转二进制
+module gray2bin
+#(
+	parameter	N  = 4						//数据位宽
+)			
+(			
+    input	[N-1 : 0]	gray, 				//格雷码
+    output 	[N-1 : 0]	bin	    			//二进制
+); 
+ 
+//从次高位到0，二进制的高位和次高位格雷码相异或
+    genvar i;
+    generate
+        for(i = 0; i < N; i = i + 1) 
+		begin: bin										//需要有名字
+			assign bin[i] = gray[N-1 : i];
+		end
+    endgenerate
+ 
+endmodule
